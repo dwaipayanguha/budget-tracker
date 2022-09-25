1b32458b9af3c6177ad5bf671880c5d7d0191f2e
dwaipayanguha10 <dwaipayanguha10@gmail.com>
2022-09-26 01:06:58 +0530

    add: state management using contextAPI

---
+++ src/context/contextReducer.js
@@ -0,0 +1,16 @@
+//Reducer => a function takes in the old state, and an action to change it to return a new state
+const contextReducer = (state, action) => {
+    let transactions
+    switch (action.type) {
+        case "DELETE_TRANSACTION":
+            transactions = state.filter((t) => t.id !== action.payload)
+            return transactions
+        case "ADD_TRANSACTION":
+            transactions = [action.payload, ...state]
+            return transactions
+        default:
+            return state
+    }
+}
+
+export default contextReducer
